cea76da0650dc53c0e3c772b0a5a52881f7a775f
Ali <alifelanv@gmail.com>
2019-08-25 09:41:05 -0500

    Add useful methods

--- classes.py
+++ classes.py
@@ -8,6 +8,15 @@ class Point:
     def __str__(self):
         return f'({self.x}, {self.y})'
 
+    def __add__(self, point):
+        return Point(self.x + point.x, self.y + point.y)
+
+    def __sub__(self, point):
+        return Point(self.x - point.x, self.y - point.y)
+
+    def __div__(self, scalar):
+        return Point(self.x / 2, self.y / 2)
+
 
 class Device:
     def __init__(self, mac, dbm, x=None, y=None):
@@ -19,11 +28,11 @@ class Device:
         return f'{self.mac}: {str(self.point)}, {self.dbm}.'
 
     def getRealDistance(self):
-        return 10 ^ ((27.55 - (20 * math.log10(2437)) + abs(self.dbm))/20)
+        return 10 ** ((27.55 - (20 * math.log10(2437)) + abs(self.dbm))/20)
 
 class Pi:
     devices = []
-    def __init__(self, x, y, id, dbmToFoot):
+    def __init__(self, x, y, id):
         self.point = Point(x, y)
         self.id = id
 
