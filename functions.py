39b8240c37bca5ee5ebef69e609ef1d18d3ef5b5
Ali <alifelanv@gmail.com>
2019-08-25 03:22:24 -0500

    Create normalize function

--- functions.py
+++ functions.py
@@ -21,3 +21,40 @@ def gen_test_case(pis=3, devices=1000):
     for pi in pis:
         pi.devices = [Device(randomMAC(), randint(-200, 0), randint(0, 100), randint(0, 100)) for i in range(devices)]
     return pis
+
+
+def normalize_snapshot(snapshot):
+    if len(snapshot) < 2:
+        return snapshot
+    x_max = x_min = snapshot[0]['x']
+    y_max = y_min = snapshot[0]['y']
+    intensity_max = intensity_min = snapshot[0]['intensity']
+    diameter_max = diameter_min = snapshot[0]['diameter']
+    for snap in snapshot[1:]:
+        if snap['x'] > x_max:
+            x_max = snap['x']
+        elif snap['x'] < x_min:
+            x_min = snap['x']
+        if snap['y'] > y_max:
+            y_max = snap['y']
+        elif snap['y'] < y_min:
+            y_min = snap['y']
+        if snap['intensity'] > intensity_max:
+            intensity_max = snap['intensity']
+        elif snap['intensity'] < intensity_min:
+            intensity_min = snap['intensity']
+        if snap['diameter'] > diameter_max:
+            diameter_max = snap['diameter']
+        elif snap['diameter'] < diameter_min:
+            diameter_min = snap['diameter']
+    normalized_snapshot = []
+    if intensity_max == intensity_min:
+        intensity_max = intensity_max + 1
+    if diameter_max == diameter_min:
+        diameter_max = diameter_max + 1
+    for snap in snapshot:
+        normalized_snapshot.append({'x': (snap['x'] - x_min) / (x_max - x_min),
+                                    'y': (snap['y'] - y_min) / (y_max - y_min),
+                                    'intensity': (snap['intensity'] - intensity_min) / (intensity_max - intensity_min),
+                                    'diameter': (snap['diameter'] - diameter_min) / (diameter_max - diameter_min)})
+    return normalized_snapshot
